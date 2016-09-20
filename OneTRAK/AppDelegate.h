f09f00e33cbf37ed66216334bbcb74217f2cb533
EnzoFurmenti <ducheenzo@mail.ru>
2016-09-20 18:25:52 +0300

    Initial Commit

---
+++ OneTRAK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneTRAK
+//
+//  Created by EnzoF on 20.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OneTRAK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OneTRAK
+//
+//  Created by EnzoF on 20.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
